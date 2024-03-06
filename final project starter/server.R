4275b30f729d492f9cfc733a77189ca84717e150
Alan Tran <tran05@outlook.com>
2024-03-05 23:55:52 -0800

    viz 2 basically done

--- final project starter/server.R
+++ final project starter/server.R
@@ -8,5 +8,17 @@ df <- read.csv("https://raw.githubusercontent.com/info-201-wi24/final-project-tr
 server <- function(input, output){
   
   # TODO Make outputs based on the UI inputs here
+  output$viz_2_plot <- renderPlotly({
+    viz_2_filtered_df <- df %>%
+      # filter for user's track name
+      filter(track_name %in% input$track_selection)
   
+      viz_2_plot <- ggplot(data = df) +
+        geom_point(mapping = 
+                     aes(x = track_name,
+                         y = !!as.name(input$viz_2_y_axis),
+                         color = artist_name,
+                         ))
+      return(ggplotly(viz_2_plot))
+  })
 }
--- final project starter/ui.R
+++ final project starter/ui.R
@@ -7,6 +7,16 @@ library(plotly)
 
 df <- read.csv("https://raw.githubusercontent.com/info-201-wi24/final-project-trnaln/main/JoinedDataset.csv")
 
+viz_2_df_colnames <- colnames(df)
+
+viz_2_df_colnames <- c(
+  "Danceability" = "danceability",
+  "Energy" = "energy", 
+  "Liveness" = "liveness",
+  "Valence" = "valence",
+  "Tempo" = "tempo"
+)
+
 overview_tab <- tabPanel("Overview",
    h1("⋆ ˚｡⋆୨୧˚⋆ ˚｡⋆ "),
    p("some explanation")
@@ -34,16 +44,24 @@ viz_1_tab <- tabPanel("Viz 1 tab title",
 ## VIZ 2 TAB INFO
 
 viz_2_sidebar <- sidebarPanel(
-  h2("Options for graph"),
+  h2("Graph Options"),
   #TODO: Put inputs for modifying graph here
+  select_widget <- 
+    selectInput(
+      inputId = "viz_2_y_axis",
+      label = "Y Axis Choices",
+      choices = viz_2_df_colnames,
+      selected = "tempo"
+  )
+  
 )
 
 viz_2_main_panel <- mainPanel(
-  h2("Vizualization 2 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
+  h2("Song Attributes"),
+  plotlyOutput(outputId = "viz_2_plot")
 )
 
-viz_2_tab <- tabPanel("Viz 2 tab title",
+viz_2_tab <- tabPanel("What Makes up a Song",
   sidebarLayout(
     viz_2_sidebar,
     viz_2_main_panel
