4f2875b1577e85349c8ed08e17657e8a858e51d1
Alan Tran <tran05@outlook.com>
2024-03-05 21:53:05 -0800

    added df

--- final project starter/server.R
+++ final project starter/server.R
@@ -2,6 +2,9 @@
 library(dplyr)
 library(ggplot2)
 library(plotly)
+
+df <- read.csv("https://github.com/info-201-wi24/final-project-trnaln/blob/0ed2a5c23a64e0940fcc5373e8d1c85a039e1380/JoinedDataset.csv")
+
 server <- function(input, output){
   
   # TODO Make outputs based on the UI inputs here
--- final project starter/ui.R
+++ final project starter/ui.R
@@ -5,6 +5,8 @@ library(dplyr)
 library(ggplot2)
 library(plotly)
 
+df <- read.csv("https://github.com/info-201-wi24/final-project-trnaln/blob/0ed2a5c23a64e0940fcc5373e8d1c85a039e1380/JoinedDataset.csv")
+
 overview_tab <- tabPanel("Overview",
    h1("Properties of a Popular Song"),
    p("some explanation")
